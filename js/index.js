c9e5474655d1d2e856dc51a2da3324114d432c4b
Will1184 <brandonwilliammg@gmail.com>
2023-09-08 02:27:14 -0600

    Updates

---
+++ js/index.js
@@ -0,0 +1,38 @@
+function change_mode(){
+document.body.classList.toggle("dark_mode");
+}
+
+function scaleCv() {
+    document.body.classList.add("scale__cv");
+  }
+    
+  function removeScale() {
+    document.body.classList.remove("scale__cv");
+  }
+  
+let areaCv = document.body;
+let resumeButton = document.getElementById("generate-pdf");
+
+let opt = {
+  margin: 0,
+  filename: "CV-BrandonGomez.pdf",
+  image: { type: "jpeg", quality: 0.98 },
+  html2canvas: { scale: 4 ,letterRendering:true},
+  pagesplit: false,
+  jsPDF: {
+    format: "a4",
+    orientation: "portrait",
+    unit: "cm"
+  }
+};
+
+// Function to call areaCv and Html2Pdf options
+function generateResume() {
+  html2pdf(areaCv, opt);
+}
+
+resumeButton.addEventListener("click", () => {  
+  scaleCv();
+  generateResume();
+  setTimeout(removeScale, 5000);
+});
